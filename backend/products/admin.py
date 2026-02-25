6382a66b83f9efd995c3fedb908bc75ed3a7ebc7
Soraya Codo <soraya.codo@epitech.eu>
2026-02-25 08:10:06 +0100

    import de amazon fashion products

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -5,13 +5,18 @@ from .models import Product
 
 class ProductResource(resources.ModelResource):
     name = fields.Field(attribute='name', column_name='title')
-    
+    category = fields.Field(attribute='category', column_name='category')
+    price = fields.Field(attribute='price', column_name='price')
+    image_url = fields.Field(attribute='image_url', column_name='image_url') 
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'category', 'image')
-        import_id_fields = ('name',) 
+        fields = ('id', 'name', 'category', 'price', 'image_url')
+        import_id_fields = ('name',) # Évite les doublons en utilisant le champ 'name' comme identifiant unique pour l'importation
+        skip_unchanged = True
 
 @admin.register(Product)
 class ProductAdmin(ImportExportModelAdmin):
     resource_class = ProductResource
-    list_display = ('name', 'price', 'category')
+    list_display = ('name', 'category', 'price')
+    search_fields = ('name', 'category')
--- backend/products/models.py
+++ backend/products/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='perfumes/', null=True, blank=True)
+    image_url = models.URLField(max_length=2000, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
